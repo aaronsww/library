ea3a608583a25c6e8a3354f061c8cbc5c85a4688
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2022-11-16 22:52:39 +0530

    inital card display

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 let myLibrary = [];
 let i;
 
-
-function book(title, author, pages, status){
+function Book(title, author, pages, status){
     this.title = title
     this.author = author
     this.pages = pages
     this.status = status
-    this.info = () => { return title+' by '+author+', '+pages+', '+status }
 }
 
-let book1 = new book('The Hobbit','J.R.R. Tolkien',295,'not read yet')
-
-
-myLibrary[0] = book1
+myLibrary[0] = new Book('The Hobbit','J.R.R. Tolkien',295,'not read yet')
 
  function addBook(){
      for(i = 1; i < 4; i++)
@@ -21,13 +16,18 @@ myLibrary[0] = book1
         let userTitle = prompt("Enter title")
         let userAuthor = prompt("Enter author")
         let userPages = prompt("Enter total pages")
-       let userStatus = prompt("Enter status")
-    //    myLibrary[i] = {userTitle, userAuthor, userPages, userStatus}
-       myLibrary[i] = new book (userTitle, userAuthor, userPages, userStatus)
+        let userStatus = prompt("Enter status")
+        myLibrary[i] = new Book (userTitle, userAuthor, userPages, userStatus)
     }
  }
 
  //addBook()   
+
+title.textContent = myLibrary[0].title;
+author.textContent = myLibrary[0].author;
+pages.textContent = myLibrary[0].pages;
+state.textContent = myLibrary[0].status;
  
-console.log(book1.title)
+ 
+  
 
