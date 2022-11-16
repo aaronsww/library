e312b19c879d88189670198a908e2997423e936b
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2022-11-16 22:24:50 +0530

    basic user prompt function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let myLibrary = [];
+let i;
+
 
 function book(title, author, pages, status){
     this.title = title
@@ -8,11 +10,24 @@ function book(title, author, pages, status){
     this.info = () => { return title+' by '+author+', '+pages+', '+status }
 }
 
-const book1 = new book('The Hobbit','J.R.R. Tolkien',295,'not read yet')
+let book1 = new book('The Hobbit','J.R.R. Tolkien',295,'not read yet')
 
-function addBook(){
-    
-}
 
-console.log(book1.info())
+myLibrary[0] = book1
+
+ function addBook(){
+     for(i = 1; i < 4; i++)
+     {
+        let userTitle = prompt("Enter title")
+        let userAuthor = prompt("Enter author")
+        let userPages = prompt("Enter total pages")
+       let userStatus = prompt("Enter status")
+    //    myLibrary[i] = {userTitle, userAuthor, userPages, userStatus}
+       myLibrary[i] = new book (userTitle, userAuthor, userPages, userStatus)
+    }
+ }
+
+ //addBook()   
+ 
+console.log(book1.title)
 
