991b86147f760ac56afaf007ca3d60f290919c65
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2022-11-19 00:51:13 +0530

    read button can now toggle

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let myLibrary = [];
 let i;
+let c = 1;
+let d = 1;
 
 const mainContainer = document.querySelector(".mainContainer")
 function Book(title, author, pages, status){
@@ -9,7 +11,7 @@ function Book(title, author, pages, status){
     this.status = status
 }
 
-myLibrary[0] = new Book('The Hobbit','J.R.R. Tolkien','295 pages','not read yet')
+myLibrary[0] = new Book('The Hobbit','J.R.R. Tolkien','295 pages','Not read')
 
  /*function addBook(){
      for(i = 1; i < 2; i++)
@@ -28,6 +30,7 @@ title.textContent = myLibrary[0].title;
 author.textContent = myLibrary[0].author;
 pages.textContent = myLibrary[0].pages;
 state.textContent = myLibrary[0].status;
+removeBtn.textContent = "Remove from list"
 
 let popup = document.getElementById("popUp");
 
@@ -46,29 +49,63 @@ create.addEventListener('click', () => {
     const parentBook = document.createElement('div');
 
     const childTitle = document.createElement('div');
+    childTitle.setAttribute('id','title');
     const bookName = document.querySelector("#bookName")
     childTitle.textContent = bookName.value
 
     const childAuthor = document.createElement('div');
+    childAuthor.setAttribute('id','author');
     const authorName = document.querySelector("#authorName")
     childAuthor.textContent = authorName.value
 
     const childPages = document.createElement('div');
+    childPages.setAttribute('id','pages');
     const totalPages = document.querySelector("#totalPages")
-    childPages.textContent = totalPages.value
+    childPages.textContent = `${totalPages.value} pages`;
 
-    const childStatus = document.createElement('div');
+    const childStatus = document.createElement('button');
+    childStatus.setAttribute('id','state');
     const readStatus = document.querySelector("#readStatus")
     childStatus.textContent = readStatus.value
+    childStatus.addEventListener('click', () => { 
+        d++
+    if(d%2==0)
+    {
+        childStatus.setAttribute("style", "background-color: #52b788;");
+        childStatus.textContent = "Read"
+    } 
+    else  
+    {
+        childStatus.setAttribute("style", "background-color: #f5999c;");
+        childStatus.textContent = "Not read"
+    }
+     })
    
+    const removeOption = document.createElement('button');
+    removeOption.setAttribute('id','removeBtn');
+    removeBtn.textContent = "Remove from list"
+
     parentBook.appendChild(childTitle)
     parentBook.appendChild(childAuthor)
     parentBook.appendChild(childPages)
     parentBook.appendChild(childStatus) 
+    parentBook.appendChild(removeBtn)
     parentBook.classList.add("bookItself");
 
     mainContainer.appendChild(parentBook)
 })
 
-
-
+const stateBtn = document.querySelector("#state")
+stateBtn.addEventListener('click', () => { 
+    c++
+    if(c%2==0)
+    {
+        stateBtn.setAttribute("style", "background-color: #52b788;");
+        stateBtn.textContent = "Read"
+    } 
+    else  
+    {
+        stateBtn.setAttribute("style", "background-color: #f5999c;");
+        stateBtn.textContent = "Not read"
+    }
+ })
